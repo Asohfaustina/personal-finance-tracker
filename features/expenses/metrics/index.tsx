d44432f001087d10cef731cafb2794517876ddb1
sylvahh <sylvesterchambers03@gmail.com>
2024-12-03 22:10:59 +0100

    chore: added animation to chart display

--- features/expenses/metrics/index.tsx
+++ features/expenses/metrics/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "@/components/theme";
+import { Animated, Easing } from "react-native";
 import * as React from "react";
 import Chart from "./chart";
 import useMetrics from "./use-metrics";
@@ -7,14 +7,35 @@ import { AppButton } from "@/components/app-button";
 import { styles } from "./styles";
 import { globalStyles } from "@/styles/global.styles";
 import Filter from "./filter";
+import { Text, View } from "@/components/theme";
 
 export default React.memo(function Metrics() {
 	const { isFetching, isError, error, data, month, setMonth } = useMetrics();
 	const [open, setOpen] = React.useState(false);
+	const animationValue = React.useRef(new Animated.Value(0)).current;
 
-	const toggle = () => setOpen((prev) => !prev);
+	const toggle = () => {
+		setOpen((prev) => !prev);
+		Animated.timing(animationValue, {
+			toValue: open ? 0 : 1,
+			duration: 200,
+			easing: Easing.ease,
+			useNativeDriver: false,
+		}).start();
+	};
+
+	const animatedHeight = animationValue.interpolate({
+		inputRange: [0, 1],
+		outputRange: [0, 300], // Adjust the height based on your chart size
+	});
+
+	const animatedOpacity = animationValue.interpolate({
+		inputRange: [0, 1],
+		outputRange: [0, 1],
+	});
+
 	return (
-		<View style={[open && styles.container]}>
+		<View >
 			<View
 				style={[
 					styles.chartHeaderBox,
@@ -27,14 +48,18 @@ export default React.memo(function Metrics() {
 					<Text type="link"> {open ? "Hide" : "Show"} Chart</Text>
 				</AppButton>
 			</View>
-			{open && (
-				<View style={styles.container}>
-					<Filter month={month} setMonth={setMonth} />
-					<Render isLoading={isFetching} isError={isError} error={error}>
-						{!data ? <Text> No Chart Data Recorded Yet</Text> : <Chart chartItems={data} />}
-					</Render>
-				</View>
-			)}
+			<Animated.View
+				style={{
+					height: animatedHeight,
+					opacity: animatedOpacity,
+					overflow: "hidden",
+				}}
+			>
+				<Filter month={month} setMonth={setMonth} />
+				<Render isLoading={isFetching} isError={isError} error={error}>
+					{!data ? <Text> No Chart Data Recorded Yet</Text> : <Chart chartItems={data} />}
+				</Render>
+			</Animated.View>
 		</View>
 	);
 });
